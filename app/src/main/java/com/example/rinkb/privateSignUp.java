6248b17408588f39238f4cde9e9383dd4689ae8b
hubblin <43435547+hubblin@users.noreply.github.com>
2020-08-13 16:33:29 +0900

    change privateSignUp
    비밀번호 체크
    디비에 값 바뀌어 들어가는거 체크
    빈 값 있으면 화면 안넘어가게 수정
    아이디 중복확인 후 edittext비활성화

--- app/src/main/java/com/example/rinkb/privateSignUp.java
+++ app/src/main/java/com/example/rinkb/privateSignUp.java
@@ -22,18 +22,20 @@ import java.net.URL;
 
 public class privateSignUp extends AppCompatActivity {
 
+    static int checkCode = 0;
+    EditText editEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_private_sign_up);
-
+        editEmail =(EditText)findViewById(R.id.edit_sigup_email);
         final Button btnSignUp = (Button)findViewById(R.id.btnSignUp);
         final LinearLayout page1 =(LinearLayout)findViewById(R.id.page1);
         final LinearLayout page2 =(LinearLayout)findViewById(R.id.page2);
         final LinearLayout page3 =(LinearLayout)findViewById(R.id.page3);
         final Button btnBack = (Button)findViewById(R.id.btnBack);
         final Button btnCheck  = (Button)findViewById(R.id.btn_dup_check);
-        final EditText editEmail = (EditText)findViewById(R.id.edit_sigup_email);
         final EditText editPwd = (EditText)findViewById(R.id.edit_sigup_pwd);
         final EditText editChPwd = (EditText)findViewById(R.id.edit_sigup_chpwd);
         final EditText editName = (EditText)findViewById(R.id.edit_sigup_name);
@@ -42,6 +44,11 @@ public class privateSignUp extends AppCompatActivity {
         final EditText editWorkCompany = (EditText)findViewById(R.id.edit_sigup_work_company);
         final EditText editWorkTeam = (EditText)findViewById(R.id.edit_sigup_work_team);
         final EditText editWorkPosition = (EditText)findViewById(R.id.edit_sigup_work_position);
+
+
+        //아이디 중복 체크를 실행했는지를 체크하는 코드
+
+
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,14 +81,30 @@ public class privateSignUp extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(page1.getVisibility()==View.VISIBLE){
-                    page1.setVisibility(View.GONE);
-                    page2.setVisibility(View.VISIBLE);
-                    page3.setVisibility(View.GONE);
+                    if(checkCode==0){
+                        Toast.makeText(getApplicationContext(), "중복확인을 하세요", Toast.LENGTH_SHORT).show();
+
+                    }else {
+
+                        if (!editPwd.getText().toString().equals(editChPwd.getText().toString())) {
+                            Toast.makeText(getApplicationContext(), "비밀번호가 다릅니다", Toast.LENGTH_SHORT).show();
+                        } else if (editEmail.getText().toString().equals("") || editPwd.getText().toString().equals("") || editChPwd.getText().toString().equals("")) {
+                            Toast.makeText(getApplicationContext(), "빈 항목이 있습니다.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            page1.setVisibility(View.GONE);
+                            page2.setVisibility(View.VISIBLE);
+                            page3.setVisibility(View.GONE);
+                        }
+                    }
                 }else if(page2.getVisibility()==View.VISIBLE){
-                    page1.setVisibility(View.GONE);
-                    page2.setVisibility(View.GONE);
-                    page3.setVisibility(View.VISIBLE);
-                    btnSignUp.setText("가입완료!");
+                    if(editName.getText().toString().equals("")||editPhone.getText().toString().equals("")||editWorkPlace.getText().toString().equals("")){
+                        Toast.makeText(getApplicationContext(),"빈 항목이 있습니다.", Toast.LENGTH_SHORT).show();
+                    }else {
+                        page1.setVisibility(View.GONE);
+                        page2.setVisibility(View.GONE);
+                        page3.setVisibility(View.VISIBLE);
+                        btnSignUp.setText("가입완료!");
+                    }
                 }else{
                     //회원가입 코드 !
                     String emailTemp = editEmail.getText().toString();
@@ -94,7 +117,7 @@ public class privateSignUp extends AppCompatActivity {
                     String workTeamTemp=editWorkTeam.getText().toString();
                     String workPositionTemp=editWorkPosition.getText().toString();
                     new RestAPITaskSignUp("http://101.101.161.189/api/index.php/linkb_member/insert_member",
-                            emailTemp,pwdTemp,nameTemp,phoneNumberTemp,workPlaceTemp,workCompanyTemp,workTeamTemp,workPositionTemp).execute();
+                            emailTemp,pwdTemp,nameTemp,phoneNumberTemp,workCompanyTemp,workPlaceTemp,workTeamTemp,workPositionTemp).execute();
 
                 }
 
@@ -181,6 +204,10 @@ public class privateSignUp extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "중복체크 실패", Toast.LENGTH_SHORT).show();
             }else if(code.equals("200")){
                 Toast.makeText(getApplicationContext(), "중복체크 성공", Toast.LENGTH_SHORT).show();
+                checkCode = 1;
+                editEmail.setEnabled(false);
+                editEmail.setClickable(false);
+                editEmail.setFocusable(false);
             }
             else if(code.equals("206")){
                 Toast.makeText(getApplicationContext(), "API Key가 틀렸습니다.", Toast.LENGTH_SHORT).show();
@@ -188,7 +215,6 @@ public class privateSignUp extends AppCompatActivity {
             else if(code.equals("500")){
                 Toast.makeText(getApplicationContext(), "API 실행 중 시스템에서 발생한 에러입니다.", Toast.LENGTH_SHORT).show();
             }
-
         }
 
 
