4888aef2262dd1cc1571ea74bcd136cee4a59d69
VioletBeach <63458653+VioletBeach@users.noreply.github.com>
2020-08-15 06:49:16 +0900

    메인화면 추가
    BottomNavigationView 진행 중 일부 게시

---
+++ app/src/main/java/com/example/rinkb/mainSearchFrag.java
@@ -0,0 +1,19 @@
+package com.example.rinkb;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class mainSearchFrag extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_logined_main_frag2, container, false);
+    }
+}
