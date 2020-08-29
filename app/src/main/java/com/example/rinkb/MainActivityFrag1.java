64dc33cd28c51d3f3403d16e139bab5608a0ea95
hubblin <43435547+hubblin@users.noreply.github.com>
2020-08-29 12:17:11 +0900

    ...

--- app/src/main/java/com/example/rinkb/MainActivityFrag1.java
+++ app/src/main/java/com/example/rinkb/MainActivityFrag1.java
@@ -2,14 +2,25 @@ package com.example.rinkb;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import java.util.ArrayList;
+
 public class MainActivityFrag1 extends Fragment {
-    ViewPager viewPager;
 
     public MainActivityFrag1(){
 
@@ -17,6 +28,22 @@ public class MainActivityFrag1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.acitivity_main_frag1, container, false);
+
+
+
+
+
+
+
+
+
         return view;
     }
+
+
+
+
+
+
+
 }
